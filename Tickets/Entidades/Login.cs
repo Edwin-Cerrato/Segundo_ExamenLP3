fe16278f890a2ca653765611575c3e1583ce5048
Edwin-Cerrato <fercerrato2020@gmail.com>
2023-03-23 10:34:42 -0600

    Creación de Clase Usuario Y Login
    Primer Comit

---
+++ Tickets/Entidades/Login.cs
@@ -0,0 +1,30 @@
+﻿namespace Entidades
+{
+    public class Login
+    {
+        //Propiedades
+        public string CodigoUsuario { get; set; }
+
+        public string Contrasena { get; set; }
+
+
+        //Constructor Vacio
+        public Login()
+        {
+        }
+
+        //Constructores de las Propiedades
+        public Login(string codigoUsuario, string contrasena)
+        {
+            CodigoUsuario = codigoUsuario;
+            Contrasena = contrasena;
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Tickets/Entidades/Usuario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Entidades
+{
+    public class Usuario
+    {
+
+        //Propiedades
+        public string CodigoUsuario { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Contrasena { get; set; }
+
+        public string Correo { get; set; }
+
+        public DateTime FechaCreacion { get; set; }
+        public bool EstaActivo { get; set; }
+
+
+        //Constructor Vacio
+        public Usuario()
+        {
+        }
+
+
+        //Constructor de Propiedades
+        public Usuario(string codigoUsuario, string nombre, string contrasena, string correo, DateTime fechaCreacion, bool estaActivo)
+        {
+            CodigoUsuario = codigoUsuario;
+            Nombre = nombre;
+            Contrasena = contrasena;
+            Correo = correo;
+            FechaCreacion = fechaCreacion;
+            EstaActivo = estaActivo;
+        }
+    }
+}
